4ecb77f63ba7052d288b7484d766f07072b26a0c
nekisse <lsh891224@gmail.com>
2018-09-17 17:42:57 +0900

    import.sql 추가

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,7 +7,7 @@ INSERT INTO PORTFOLIO (IDX, TITLE, CONTENT, IMAGE) VALUES (5,'2017 </br> Hello W
 
 INSERT INTO PORTFOLIO (IDX, TITLE, CONTENT, IMAGE) VALUES (3,'KCTC 연습', 'KCTC  대회룰에 맞춰 </br> 연습하려고 준비 해 놓은 사진입니다. </br>  8세트 각 세트당 세잔의 커피 중 </br>  맛이 다른 한잔의 커피를 골라내야 합니다. ', 'img/portfolio/kctc.png');
 
-INSERT INTO PORTFOLIO (IDX, TITLE, CONTENT, IMAGE) VALUES (2,'2014 BAOK CHAMPOINSHIP<br> Barista Association</br> of </br> Korea</br>', '2014 BAOK 참가 첫 바리스타 대회 참가!</br> 첫 대회 인 것 만큼 많이 서툴렀지만 </br> 대회준비를 하면서 무엇을 공부해야 할지 알게 되었고 커피에 대해 좀 더 많은 것을 배웠습니다. ', 'img/portfolio/coffee1.png');
+INSERT INTO PORTFOLIO (IDX, TITLE, CONTENT, IMAGE) VALUES (2,'2014 BAOK CHAMPOINSHIP<br> Barista Association</br> of </br> Korea</br>', '2014 BAOK 참가 첫 바리스타 대회 참가!</br> 첫 대회 인 것 만큼 많이 서툴렀지만 </br> 대회준비를 하면서 무엇을 공부해야 할지 알게 되었고</br> 커피에 대해 좀 더 많은 것을 배웠습니다. ', 'img/portfolio/coffee1.png');
 INSERT INTO PORTFOLIO (IDX, TITLE, CONTENT, IMAGE) VALUES (1,'2015, 2016 WCCK KCTC', '2015 WCCK KCTC 본선 첫 KCTC참가에 운도 따라 주어서 본선진출을 했지만 아쉽게 떨어졌습니다.', 'img/portfolio/2015kctc.png');
 
 
